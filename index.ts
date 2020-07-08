ad15f9acd30302a81acce5cbf8fcd405c20fc1c8
Radek Kysely <radek@kysely.org>
2020-07-08 09:35:23 +0200

    (add) import

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ export { loggingMiddleware } from './middleware/logging';
 export { authenticationMiddleware } from './middleware/authentication';
 export { requestContextMiddleware } from './middleware/request-context';
 export { sortQueryParser } from './middleware/sort-query-parser';
+export { queryArrayParserMiddlware } from './middleware/query-array-parser';
 export { apiErrorMiddleware } from './middleware/api-error';
 export { paginationLinkMiddleware } from './middleware/pagination-link';
 export { validate } from './middleware/validation';
